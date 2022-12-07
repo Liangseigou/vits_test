3aec6119a1e51d9f59df333deb2b0780bc926539
Liangseigou <82559477+Liangseigou@users.noreply.github.com>
2022-12-07 17:39:11 +0900

    Update cleaners.py

--- text/cleaners.py
+++ text/cleaners.py
@@ -101,5 +101,5 @@ def english_cleaners2(text):
 
 def japanese_cleaner(text):
   Jtext=phones=pyopenjtalk.g2p(text,kana=True)
-  ph = phonemize(Jtext, strip=False, njobs=1, backend='espeak', language='ja', preserve_punctuation=True, language_switch="remove-flags")
-  return ph
+  phonems = phonemize(Jtext, strip=False, njobs=1, backend='espeak', language='ja', preserve_punctuation=True, language_switch="remove-flags")
+  return phonems
